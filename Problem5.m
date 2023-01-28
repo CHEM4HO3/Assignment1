3dc6025d933db58242aca076cfc1f7532f8f7b86
aronm99 <markanda@mcmaster.ca>
2023-01-28 16:28:59 -0500

    Add labels to graph and rsqared values

--- Problem5.m
+++ Problem5.m
@@ -14,11 +14,20 @@ sumHW = sum(height.*weight);
 A = [size(height,1) sumH; sumH sumH2];
 B = [sumW; sumHW];
 coeff = A\B;
-line = @(x) coeff(1,1) + coeff(2,1)*x;
+line = @(x) coeff(1,1) + coeff(2,1).*x;
+
+yHat = line(height); 
+yBar = sumW/size(weight,1);
+rSq = 1 - (sum((yHat-weight).^2))/(sum((weight-yBar).^2))
+
 figure
 scatter(height,weight);
 hold
 fplot(line,[min(height), max(height)])
+title("NBA Hight Vs Weight with Linear Regression");
+xlabel("height")
+ylabel("weight")
+legend('', sprintf('w = %.3f + %.3fh', coeff(1,1), coeff(2,1)))
 
 sumH3 = sum(height.^3);
 sumH4 = sum(height.^4);
@@ -27,8 +36,16 @@ sumH2W = sum((height.^2).*weight);
 A2 = [size(height,1) sumH sumH2; sumH sumH2 sumH3; sumH2 sumH3 sumH4];
 B2 = [sumW; sumHW; sumH2W];
 coeff2 = A2\B2;
-poly = @(x) coeff2(1,1) + coeff2(2,1)*x + coeff2(3,1)*(x^2);
+poly = @(x) coeff2(1,1) + coeff2(2,1).*x + coeff2(3,1).*(x.^2);
+
+yHat2 = poly(height); 
+rSq2 = 1 - (sum((yHat2-weight).^2))/(sum((weight-yBar).^2))
+
 figure
 scatter(height,weight);
 hold
 fplot(poly,[min(height), max(height)])
+title("NBA Hight Vs Weight with Quadratic Regression");
+xlabel("height")
+ylabel("weight")
+legend('', sprintf('w = %.3f + %.3fh + %.3fh^2', coeff(1,1), coeff(2,1), coeff2(3,1)))
